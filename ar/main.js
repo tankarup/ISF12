496464b24d136942e5fb89490194d78a5792e78e
tankarup <69491808+tankarup@users.noreply.github.com>
2024-05-06 23:29:04 +0900

    Update main.js

---
+++ ar/main.js
@@ -0,0 +1,12 @@
+window.addEventListener("load",function() {
+	on_load();
+});
+
+function on_load(){
+	document.getElementById('seichi').addEventListener('click', open('pdf/seichi.pdf'));
+	document.getElementById('maincommu').addEventListener('click', open('pdf/maincommu.pdf'));
+}
+
+function open(url){
+	window.open(url, '_blank');
+}
